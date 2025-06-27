4fd30e80b7cb1ad6f058f7d7bdf901ad69a4750f
Srdjan Stojanac <stojanac.srdjan@gmail.com>
2025-06-27 18:21:28 +0200

    Header and Login page for delovodnik frontend

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import hamburger from "../assets/hamburger36.png";
+import "../index.css"
+
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleMenu() {
+        setIsOpen(prev => !prev);
+    }
+    return (
+        <>
+            <header className="bg-indigo-800 text-white p-4 ">
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-4">
+                        <img src={logo} alt="rgz-logo" className="w-15 h-15" />
+                        <h3 className="text-md font-semibold">RGZ Delovodnik</h3>
+                    </div>
+                    {/* Hamburger dugme */}
+                    <button
+                        onClick={toggleMenu}
+                        className="block md:hidden focus:outline-none"
+                    >
+                        <img src={hamburger} alt="hamburger" className="w-8 h-8" />
+                    </button>
+
+                    {/* Desktop meni */}
+                    <nav className="hidden md:flex gap-6">
+                        <a href="#" className="hover:underline">Početna</a>
+                        <a href="#" className="hover:underline">Statistika</a>
+                        <a href="#" className="hover:underline">Korisnici</a>
+                    </nav>
+                </div>
+
+                {/* Mobile meni */}
+                {isOpen && (
+                    <div className="mt-4 md:hidden flex flex-col gap-2" >
+                        <a href="#" className="hover:underline">Početna</a>
+                        <a href="#" className="hover:underline">Statistika</a>
+                        <a href="#" className="hover:underline">Korisnici</a>
+                    </div>
+                )}
+            </header >
+        </>
+    )
+}
